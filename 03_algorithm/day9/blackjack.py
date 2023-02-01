1d1f0df95bfdc3a4288c5c9504f67c0e6c030dff
KDT live <kdt-live@hphk.kr>
2023-02-01 11:50:30 +0900

    수업

---
+++ 03_algorithm/day9/blackjack.py
@@ -0,0 +1,18 @@
+
+N = 5
+M = 21
+
+# O(N^3)
+for i in range(5):
+    for j in range(5):
+        for k in range(5):
+            # i, j, k => 비교문 필요 
+             pass
+
+# O(N^3)
+for i in range(N-2):
+    for j in range(i+1, N-1):
+        for k in range(j+1, N):
+            pass 
+
+

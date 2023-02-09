c09aed71462cb3e3a70d6463ad63023e112b6b17
KDT live <kdt-live@hphk.kr>
2023-02-09 13:04:55 +0900

    add db 01

---
+++ 04_db/01_single_table_queries.sql
@@ -0,0 +1,62 @@
+SELECT
+	lastName 
+FROM 
+	employees;
+    
+SELECT 
+	lastName, firstName
+FROM
+	employees;
+    
+SELECT
+	*
+FROM
+	employees;
+    
+SELECT
+	firstName AS '이름'
+FROM 
+	employees;
+    
+SELECT 
+	productCode, 
+    quantityOrdered * priceEach AS '주문 총액'
+FROM
+	orderdetails;
+
+-- 주석
+-- SELECT 
+-- *
+-- FROM
+-- 	orderdetails;
+    
+SELECT
+	firstName
+FROM
+	employees
+ORDER BY
+	firstName;
+
+SELECT
+	firstName
+FROM
+	employees
+ORDER BY
+	firstName DESC;
+    
+SELECT
+	lastName, firstName
+FROM
+	employees
+ORDER BY
+	lastName DESC, firstName ASC;
+
+SELECT 
+	productCode, 
+    quantityOrdered * priceEach AS totalsales
+FROM
+	orderdetails
+ORDER BY
+	totalsales DESC;
+    
+    

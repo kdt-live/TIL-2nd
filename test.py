8a69e50eab2395f208285a1771f6fa2be1c6d045
harryjjun <harry@hphk.com>
2023-03-14 14:17:26 +0900

    fixed 03 js

--- test.py
+++
@@ -1,9 +0,0 @@
-number_list = [1, 2, 3, 4, -5]
-
-# result : 0
-result = 0
-# number_list를 반복해서 number
-for number in number_list:
-    # number를 result에 더함
-    result = result + number
-print(result)

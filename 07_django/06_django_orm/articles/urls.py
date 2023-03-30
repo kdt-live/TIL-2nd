35984ab40f1f8c901421766bd6b19fc8ca3bb485
KDT live <kdt-live@hphk.kr>
2023-03-30 12:09:08 +0900

    add ORM UD

--- 07_django/06_django_orm/articles/urls.py
+++ 07_django/06_django_orm/articles/urls.py
@@ -5,9 +5,17 @@ from . import views
 app_name = 'articles'
 urlpatterns = [
     path('', views.index, name='index'),
-    path('<int:pk>/', views.detail, name='detail'),
     path('new/', views.new, name='new'),
     path('create/', views.create, name='create'),
+
+    # 데이터 조회에 대한 URL 패턴
+    path('<int:pk>/', views.detail, name='detail'),
+    # 데이터 수정 페이지에 대한 URL 패턴
+    path('<int:article_pk>/edit/', views.edit, name='edit'),
+    # 데이터 삭제에 대한 URL 패턴
+    path('<int:artilce_pk>/delete/', views.delete, name='delete'),
+    # 데이터 수정 로직에 대한 URL 패턴
+    path('<int:article_pk>/update/', views.update, name='update'),
 ]
 
 
--- 07_django/06_django_orm/articles/views.py
+++ 07_django/06_django_orm/articles/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Article
 
 
@@ -10,6 +10,8 @@ def index(request):
     context = {
         'articles': articles,
     }
+
+    # context - 템플릿에 데이터와 함께 렌더링
     return render(request, 'articles/index.html', context)
 
 
@@ -29,8 +31,8 @@ def new(request):
 def create(request):
     # new에서 보낸 사용자 데이터를 받아서 변수에 할당
     # print(request.GET)
-    title = request.GET.get('title')
-    content = request.GET.get('content')
+    title = request.POST.get('title')
+    content = request.POST.get('content')
     
     # 받은 데이터를 DB에 저장
     # 1
@@ -48,4 +50,61 @@ def create(request):
     # Article.objects.create(title=title, content=content)
 
     # 결과 페이지 반환
-    return render(request, 'articles/create.html')
+    # return render(request, 'articles/create.html')
+
+    # 결과 페이지(전체 조회 템플릿) 반환
+    
+    # articles = Article.objects.all()
+    # context = {
+    #     'articles': articles,
+    # }
+    # return render(request, 'articles/index.html', context)
+
+    # 이동할 주소(URL)를 사용자에게 응답
+    # return reirect('articles:index')
+
+    # 생성한 글의 단일 조회(DETAIL) 주소(URL)로 이동 응답
+    return redirect('articles:detail', article.pk)
+
+
+def delete(request, artilce_pk):
+    # 삭제할 데이터 조회
+    article = Article.objects.get(pk=artilce_pk)
+
+    # 조회한 데이터 삭제(DELETE)
+    article.delete()
+
+    # 전체 조회 페이지 이동
+    return redirect('articles:index')
+
+
+def edit(request, article_pk):
+    # 수정 페이지(Edit) 입력 요소에 미리 값을 채우기 위한 데이터 조회
+    article = Article.objects.get(pk=article_pk)
+
+    context = {
+        'article': article
+    }
+
+    return render(request, 'articles/edit.html', context)
+
+
+def update(request, article_pk):
+    # 수정 작업 과정
+    # 1. 데이터 조회 -> delete
+    article = Article.objects.get(pk=article_pk)
+
+    # 2. 데이터 수정
+    # 2-1. 사용자가 입력한 form 데이터 할당 -> create
+    title = request.POST.get('title')
+    content = request.POST.get('content')
+
+    # 2-2. 조회한 데이터(article)의 필드 값 변경
+    article.title = title
+    article.content = content
+
+    # 3. 데이터 저장 -> create
+    article.save()
+
+    return redirect('articles:detail', article.pk)
+

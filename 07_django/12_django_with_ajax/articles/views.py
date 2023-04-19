333b0696c00baa8855e84155d32a4d3b8f5dedbc
harryjjun <harry@hphk.com>
2023-04-19 14:01:40 +0900

    set like ajax

--- 07_django/12_django_with_ajax/articles/views.py
+++ 07_django/12_django_with_ajax/articles/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from django.http import JsonResponse
 from .models import Article, Comment
 from .forms import ArticleForm, CommentForm
 
@@ -96,11 +97,17 @@ def comment_delete(request, article_pk, comment_pk):
     return redirect('articles:detail', article_pk)
 
 
+@login_required
 def likes(request, article_pk):
     article = Article.objects.get(pk=article_pk)
 
     if request.user in article.like_users.all():
         article.like_users.remove(request.user)
+        is_liked = False
     else:
         article.like_users.add(request.user)
-    return redirect('articles:index')
+        is_liked = True
+    context = {
+        'is_liked': is_liked,
+    }
+    return JsonResponse(context)
